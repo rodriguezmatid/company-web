7bbe06472df5166b6a6d0054545f4e18e784e694
Matias Rodriguez <matias.demian.r@gmail.com>
2023-04-06 11:00:32 -0300

    fix

---
+++ webempresa/blog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Name")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created date")
+    updated = models.DateField(auto_now=True, verbose_name="Edited date")
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Title")
+    content = models.TextField(verbose_name="Content")
+    published = models.DateTimeField(verbose_name="Publication date", default = now())
+    image = models.ImageField(verbose_name="Image", upload_to="blog", null=True, blank=True)
+    author = models.ForeignKey(User, verbose_name="Author", on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, verbose_name="Categories")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created date")
+    updated = models.DateField(auto_now=True, verbose_name="Edited date")
+
+    class Meta:
+        verbose_name = "entrace"
+        verbose_name_plural = "entrances"
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
